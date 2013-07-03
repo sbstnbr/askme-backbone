33b9c040b4959ce3053ac46fd81b4f100a6e5529
Alejandro Leon <a.leon.escalera@accenture.com>
2013-07-03 17:38:38 -0500

    Init schedules module object

--- js/lib/App.js
+++ js/lib/App.js
@@ -1,11 +1,18 @@
 define(
     ['backbone'],
     function (Backbone) {
+        var schedules = new (Backbone.View.extend({
+                Collections : {},
+                Models : {},
+                Views : {}
+            }))({el : document.body});
+
         var init = function() {
             console.log('Init application');
         }
 
         return {
+            schedules : schedules,
             init : init
         }
     }
