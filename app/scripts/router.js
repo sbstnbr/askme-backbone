c9d2bc2f94cb72d4e12f4525e7769f71341f6d69
Martin Lofaj <martin.lofaj@accenture.com>
2015-03-11 09:01:28 +0100

    moved social into the app
    Summary:
    - moved the yammer plugin into its own view
    - fixed bug with calendar rendering
    
    Test Plan:
    - open app
    - navigate to social
    - log in yammer
    - (optional) try diffenet route combinations if everything renders correctly
    
    Reviewers: #aowp-ba, dominika.hurajtova, slavomir.kubacka
    
    Reviewed By: #aowp-ba, dominika.hurajtova, slavomir.kubacka
    
    Subscribers: dominika.hurajtova
    
    JIRA Issues: larc-441
    
    Differential Revision: http://23.23.125.207/D572

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -3,24 +3,28 @@ define([
     'backbone',
     'views/AppView',
     'views/IndexView',
+    'views/SocialView',
     'modules/Schedule/controller',
     'modules/Questions/controller'
-], function(_, Backbone, AppView, IndexView, ScheduleController, QuestionsController) {
+], function(_, Backbone, AppView, IndexView, SocialView, ScheduleController, QuestionsController) {
     'use strict';
 
     var initialize = function() {
 
         var appView = new AppView;
         var indexView = new IndexView;
+        var socialView = new SocialView;
 
         var appRouter = new (Backbone.Router.extend({
             routes: {
                 'schedule(/)': 'schedules',
                 'questions(/)': 'questions',
+                'social(/)': 'social',
                 '(*actions)(/)': 'defaultAction'
             },
 
             schedules: function() {
+                indexView.render();
                 indexView.showOnlySchedule();
             },
 
@@ -30,7 +34,12 @@ define([
 
             defaultAction: function(actions) {
                 indexView.render();
+            },
+
+            social: function() {
+                socialView.render();
             }
+
         }));
 
         Backbone.history.start();
---
+++ app/scripts/views/SocialView.js
@@ -0,0 +1,35 @@
+define([
+    'underscore',
+    'backbone',
+    'handlebars',
+    'text!../../templates/social.hbs',
+], function(_, Backbone, Handlebars, SocialTemplate) {
+    'use strict';
+
+    return Backbone.View.extend({
+
+        el: $('.contentWrapper'),
+
+        template: Handlebars.compile(SocialTemplate),
+
+        render: function() {
+            this.$el.html(this.template({}));
+
+            // library loaded outside the requirejs via CDN
+            yam.connect.embedFeed({
+                container: '#embedded-feed',
+                network: "accenture.com",
+                feedType: "group",
+                feedId: "2286518"
+            });
+
+            yam.on('/embed/feed/loadingCompleted', function () {
+                setTimeout(function () {
+                    clearInterval(fixHeight);
+                }, 5000);
+            });
+
+            return this;
+        }
+    });
+});
