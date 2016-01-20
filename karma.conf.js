6daf1fa67a4fd59260f2d591d7a273267645670d
Martin Lofaj <martin.lofaj@accenture.com>
2016-01-20 01:33:44 +0100

    added code coverage via istanbu

--- karma.conf.js
+++ karma.conf.js
@@ -45,13 +45,14 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'app/scripts/**/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'junit'],
+    reporters: ['progress', 'junit', 'coverage'],
 
 
     junitReporter: {
