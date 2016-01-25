eea01ffe7ed5dfe2efc58ce4ccfd3ef5ce487108
Martin Lofaj <martin.lofaj@accenture.com>
2016-01-25 23:39:52 +0100

    error handling in tests added

--- features/step_definitions/twitter-integration.steps.js
+++ features/step_definitions/twitter-integration.steps.js
@@ -17,7 +17,10 @@ module.exports = function() {
       .waitForVisible('.questionItem', 3000)
       .setValue('#question-textarea', 'This is my new question')
       .submitForm('#addQuestionForm')
-      .end(function() { callback() });
+      .catch(callback.fail)
+      .end(function() { callback(); });
+
+
   });
 
   this.When(/^I press the Tweet button$/, function (callback) {
@@ -27,7 +30,8 @@ module.exports = function() {
       .url('http://localhost:9999')
       .waitForVisible('.questionItem', 3000)
       .click('#twitter-button')
-      .end(function() { callback() });
+      .catch(callback.fail)
+      .end(function() { callback(); });
   });
 
   this.Then(/^I should see a tweet with my question ready to be tweeted$/, function (callback) {
@@ -36,6 +40,7 @@ module.exports = function() {
       .init()
       .url('https://twitter.com/intent/tweet?text=This%20is%20my%20new%20question&hashtags=LAS')
       .waitForVisible('.button', 3000)
-      .end(function() { callback() });
+      .catch(callback.fail)
+      .end(function() { callback(); });
   });
 }
